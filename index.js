833c30f40731adb64e7b27bc72fe2b3c7c2f86ff
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 04:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function updateObjectWithKeyAndValue(object,key,value){
- var clone = Object.assign({},object);
-      clone[key]=value;
+ var clone = Object.assign({},object); // first I created a variable to hold the new object without modifying the original
+      clone[key]=value;// I added new values to the clone, and it will print, the original values plus the new ones.
       return clone;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key]=value;
+  return object;
 }
