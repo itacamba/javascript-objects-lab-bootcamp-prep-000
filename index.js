0b11f2acf33a2a3284e79c32f01ee189d77e7d66
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 04:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 function deleteFromObjectByKey(object,key){
   var clone = Object.assign({},object);
   delete clone[key];
+  return clone;
 }
 
 function destructi
